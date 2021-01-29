f399b64d0c35f2dfcc3b4db3d0936b80a86dffd9
Jordan Wright <jmwright798@gmail.com>
2021-01-28 21:43:17 -0600

    Added URL scheme for pubsub topic

--- terraform/pypi.tf
+++ terraform/pypi.tf
@@ -21,6 +21,10 @@ resource "google_cloud_run_service" "run-pypi" {
     spec {
       containers {
         image = "gcr.io/${var.project}/feeds-pypi"
+        env {
+          name  = "OSSMALWARE_TOPIC_URL"
+          value = "gcppubsub://${google_pubsub_topic.feed-topic.id}"
+        }
       }
     }
   }
