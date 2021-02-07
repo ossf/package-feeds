3e1cb7a2a175d2ebaab5745f93cc20a0f64f3eda
Dan Lorenc <dlorenc@google.com>
2021-02-07 15:03:50 -0600

    Make the images we run installation in look a little more realistic.
    The jobs now run as a non-root user in a real home directory.

--- analysis/python-runner.sh
+++ analysis/python-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for pkg in $(cat python.txt); do
-    kubectl run $pkg --image=python:3 --restart='Never' --labels=install=1 --requests="cpu=250m" -- sh -c "mkdir -p /app && cd /app && pip3 install $pkg"
+    kubectl run $pkg --image=python:3 --restart='Never' --labels=install=1 --requests="cpu=250m" -- sh -c "pip3 install $pkg"
 done
