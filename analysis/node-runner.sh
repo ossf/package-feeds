6d91f268d94e30f8c039e8e9b2153125e3a1728c
Dan Lorenc <dlorenc@google.com>
2021-02-12 10:55:57 -0600

    Remove analysis/scheduler and point to their new location.

--- analysis/node-runner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for pkg in $(cat node.txt); do
-    kubectl run $pkg --image=node --restart='Never' --labels=install=1 --requests="cpu=250m" -- sh -c "npm init -f && npm install $pkg --save"
-done
