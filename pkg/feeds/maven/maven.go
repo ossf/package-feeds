130c7cd65a065b5ce4a45a9942849573a357a25e
Yolanda Robla <info@ysoft.biz>
2024-02-19 14:53:18 +0100

    feat: add support for java
    Use sonatype API to query for updates in maven central repo
    and publish those into feeds
    
    Signed-off-by: Yolanda Robla <yolanda@stacklok.com>
    Signed-off-by: Yolanda Robla <info@ysoft.biz>

---
+++ pkg/feeds/maven/maven.go
@@ -0,0 +1,141 @@
+package maven
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ossf/package-feeds/pkg/feeds"
+)
+
+const (
+	FeedName  = "maven"
+	indexPath = "/api/internal/browse/components"
+)
+
+type Feed struct {
+	baseURL string
+	options feeds.FeedOptions
+}
+
+func New(feedOptions feeds.FeedOptions) (*Feed, error) {
+	if feedOptions.Packages != nil {
+		return nil, feeds.UnsupportedOptionError{
+			Feed:   FeedName,
+			Option: "packages",
+		}
+	}
+	return &Feed{
+		baseURL: "https://central.sonatype.com/" + indexPath,
+		options: feedOptions,
+	}, nil
+}
+
+// Package represents package information.
+type LatestVersionInfo struct {
+	Version             string `json:"version"`
+	TimestampUnixWithMS int64  `json:"timestampUnixWithMS"`
+}
+
+type Package struct {
+	Name              string            `json:"name"`
+	Namespace         string            `json:"namespace"`
+	LatestVersionInfo LatestVersionInfo `json:"latestVersionInfo"`
+}
+
+// Response represents the response structure from Sonatype API.
+type Response struct {
+	Components []Package `json:"components"`
+}
+
+// fetchPackages fetches packages from Sonatype API for the given page.
+func (feed Feed) fetchPackages(page int) ([]Package, error) {
+	// Define the request payload
+	payload := map[string]interface{}{
+		"page":          page,
+		"size":          20,
+		"sortField":     "publishedDate",
+		"sortDirection": "desc",
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding JSON: %w", err)
+	}
+
+	// Send POST request to Sonatype API.
+	resp, err := http.Post(feed.baseURL+"?repository=maven-central", "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Handle rate limiting (HTTP status code 429).
+	if resp.StatusCode == http.StatusTooManyRequests {
+		time.Sleep(5 * time.Second)
+		return feed.fetchPackages(page) // Retry the request
+	}
+
+	// Decode response.
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding response: %w", err)
+	}
+	return response.Components, nil
+}
+
+func (feed Feed) Latest(cutoff time.Time) ([]*feeds.Package, time.Time, []error) {
+	pkgs := []*feeds.Package{}
+	var errs []error
+
+	page := 0
+	for {
+		// Fetch packages from Sonatype API for the current page.
+		packages, err := feed.fetchPackages(page)
+		if err != nil {
+			errs = append(errs, err)
+			break
+		}
+
+		// Iterate over packages
+		hasToCut := false
+		for _, pkg := range packages {
+			// convert published to date to compare with cutoff
+			if pkg.LatestVersionInfo.TimestampUnixWithMS > cutoff.UnixMilli() {
+				// Append package to pkgs
+				timestamp := time.Unix(pkg.LatestVersionInfo.TimestampUnixWithMS/1000, 0)
+				packageName := pkg.Namespace + ":" + pkg.Name
+
+				newPkg := feeds.NewPackage(timestamp, packageName, pkg.LatestVersionInfo.Version, FeedName)
+				pkgs = append(pkgs, newPkg)
+			} else {
+				// Break the loop if the cutoff date is reached
+				hasToCut = true
+			}
+		}
+
+		// Move to the next page
+		page++
+
+		// Check if the loop should be terminated
+		if len(pkgs) == 0 || hasToCut {
+			break
+		}
+	}
+
+	newCutoff := feeds.FindCutoff(cutoff, pkgs)
+	pkgs = feeds.ApplyCutoff(pkgs, cutoff)
+
+	return pkgs, newCutoff, errs
+}
+
+func (feed Feed) GetName() string {
+	return FeedName
+}
+
+func (feed Feed) GetFeedOptions() feeds.FeedOptions {
+	return feed.options
+}
